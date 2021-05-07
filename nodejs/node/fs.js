2548eef00da534f185d7e46fe5288b73e85d0183
XUELUOCC <3196168302@qq.com>
2021-05-07 17:42:05 +0800

    文件流上传

--- nodejs/node/fs.js
+++ nodejs/node/fs.js
@@ -1,10 +1,10 @@
 const fs=require('fs');
 
-fs.readFile('1.txt',(err,data)=>{
+fs.readFile('./1.txt',(err,data)=>{
     if(!err){
         console.log(data);
         console.log(data.toString())
-        fs.writeFile('1-Child.txt',data+'1.txt的复制品',err=>{
+        fs.writeFile('2-Child.txt',data+'1.txt的复制品',err=>{
             if(err){
                 console.log('书写复制出错')
             }else{
